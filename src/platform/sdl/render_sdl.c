a08a7bfd5c86ef377698d94001896e3c6d2c5156
Derrick Gold <derrick.b.gold@gmail.com>
2026-08-28 14:35:06 -0700

    refactor: port action plane effects

--- src/platform/sdl/render_sdl.c
+++ src/platform/sdl/render_sdl.c
@@ -58,6 +58,8 @@ static SDL_BlendMode ToSdlBlendMode(ArRenderBlendMode blend) {
     case kArRenderBlendMode_Opaque: return SDL_BLENDMODE_NONE;
     case kArRenderBlendMode_Alpha: return SDL_BLENDMODE_BLEND;
     case kArRenderBlendMode_Add: return SDL_BLENDMODE_ADD;
+    case kArRenderBlendMode_AddPremultiplied:
+      return SDL_BLENDMODE_ADD_PREMULTIPLIED;
     case kArRenderBlendMode_Modulate: return SDL_BLENDMODE_MOD;
     case kArRenderBlendMode_Multiply: return SDL_BLENDMODE_MUL;
   }
